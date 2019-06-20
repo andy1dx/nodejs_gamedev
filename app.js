34b9459fe79beb388ae95a760c8be5868ade1a08
USER <andy.william@bit-okutama.jp>
2019-06-20 17:46:42 +0900

    making Multiple socket

---
+++ app.js
@@ -0,0 +1,49 @@
+// Import Module
+var express = require('express');
+var app = express();
+var serv = require('http').Server(app);
+
+// set url route
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/client/index.html')
+});
+app.use('/client', express.static(__dirname + '/client'));
+
+//set port to listern
+serv.listen(2004);
+console.log('server started');
+
+
+var SOCKET_LIST = {};
+ 
+var io = require('socket.io')(serv,{});
+io.sockets.on('connection', function(socket){
+    socket.id = Math.random();
+    socket.x = 0;
+    socket.y = 0;
+    socket.number = "" + Math.floor(10 * Math.random());
+    SOCKET_LIST[socket.id] = socket;
+ 
+    socket.on('disconnect',function(){
+        delete SOCKET_LIST[socket.id];
+    });
+   
+});
+ 
+setInterval(function(){
+    var pack = [];
+    for(var i in SOCKET_LIST){
+        var socket = SOCKET_LIST[i];
+        socket.x++;
+        socket.y++;
+        pack.push({
+            x:socket.x,
+            y:socket.y,
+            number:socket.number
+        });    
+    }
+    for(var i in SOCKET_LIST){
+        var socket = SOCKET_LIST[i];
+        socket.emit('newPositions',pack);
+    }
+},1000/25);
